7a3dce7ff75971842ac513fbe148fb130ae06704
efya442 <efyanajat@gmail.com>
2020-08-13 10:20:21 +0100

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import AkalGa from './week-3/Najat.js';
+
+
+import './week-3/css/components.css';
+import './week-3/css/global.css';
+import './week-3/css/layout.css';
+import './week-3/css/sample-wallpaper.css';
+
+
+function App() {
+  return (
+    <div className="App">
+
+
+
+  
+<AkalGa />
+
+    </div>
+  );
+}
+
+export default App;
